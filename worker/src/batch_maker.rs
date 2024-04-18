670f1dcafa67956fd81d14fa1cb851cb78125d76
neilgiri <neilgiri88@gmail.com>
2024-04-17 21:45:10 -0700

    change to 20 second partition

--- worker/src/batch_maker.rs
+++ worker/src/batch_maker.rs
@@ -100,7 +100,7 @@ impl BatchMaker {
         tokio::pin!(timer);
         let timer1 = sleep(Duration::from_secs(10));
         tokio::pin!(timer1);
-        let timer2 = sleep(Duration::from_secs(12));
+        let timer2 = sleep(Duration::from_secs(30));
         tokio::pin!(timer2);
         let mut current_time = Instant::now();
 
