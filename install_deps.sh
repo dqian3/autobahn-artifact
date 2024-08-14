dc332f6e15045d417c3619141c6e60218f56a6d4
neilgiri <neilgiri88@gmail.com>
2024-08-14 14:31:09 -0700

    update install script to match albertos

--- install_deps.sh
+++ install_deps.sh
@@ -1,11 +1,24 @@
-sudo apt update
-sudo apt install clang tmux
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
 
+# The following dependencies prevent the error: [error: linker `cc` not found].
+sudo apt-get -y install build-essential
+sudo apt-get -y install cmake
 
-sudo apt remove python3-pip
+# Install rust (non-interactive).
+curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env'
+rustup default stable'
+
+# This is missing from the Rocksdb installer (needed for Rocksdb).
+sudo apt-get install -y clang
+
+# Tmux if not already installed
+sudo apt-get install -y tmux
+
+sudo apt-get remove python3-pip
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 
 export PATH=$PATH:~/.local/bin
-source $HOME/.cargo/env
