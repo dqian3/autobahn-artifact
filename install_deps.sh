a934564d766732d9211b86dfdbdf815408da39cd
neilgiri <neilgiri88@gmail.com>
2024-08-14 14:19:11 -0700

    updated eps

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,11 @@
 sudo apt update
-sudo apt install clang tmux python3-pip
+sudo apt install clang tmux
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+
+sudo apt remove python3-pip
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+
 export PATH=$PATH:~/.local/bin
+source $HOME/.cargo/env
